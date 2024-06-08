d232486c13126e9eadb2f16474fd2ec59fdb4541
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-06-08 21:01:33 +0530

    Altered navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { CiMenuFries } from "react-icons/ci";
+import { GiTireIronCross } from "react-icons/gi";
 import { navLinks } from "../utils/Data";
 import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 const Navbar = () => {
      //________ STATES __________
@@ -28,18 +30,26 @@ const Navbar = () => {
      return (
          <header>
              <nav>
-               <div className="flex justify-between px-20 py-7 flex-wrap border-b-2 ">
+               <div className="flex justify-between bg-blue-950 md:bg-white text-white md:text-black px-4 md:px-20 py-7 flex-wrap md:border-b-2 ">
                     <h3>Name</h3>
-                    <div>
-                         <ul className="flex gap-8">
+                <button className="text-xl">
+                <BsSearch/>
+                </button>
+              
+                   <div className={`${isMenuOpen ? '' : 'hidden'} md:inline`}>
+                         <ul className="flex flex-col absolute right-3 bg-white text-black py-3 px-5 md:p-0  gap-8 md:flex md:flex-row md:relative duration-700">
                               {navLinks.map(({link, path}) => 
-                                  <Link to={path}>
+                                  <Link to={path} key={path}>
                                    <li>{link}</li>
                                   </Link>
                               )}
                          </ul>
                     </div>
-                    <CiMenuFries />
+                   <button onClick={toggleMenu} className={`${isMenuOpen ? 'absolute md:relative right-6 md:right-0 top-11 md:top-0 text-black' : '' }`} >
+                  { isMenuOpen ? <GiTireIronCross/> : <CiMenuFries />}
+                   </button>
+                 
+                   
                </div>
           </nav>
          </header>
