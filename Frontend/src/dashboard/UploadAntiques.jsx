2f4c287706bf480e7fb64e2991a110c03c821265
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-06-14 15:43:03 +0530

    Wrap up edit antique

--- Frontend/src/dashboard/UploadAntiques.jsx
+++ Frontend/src/dashboard/UploadAntiques.jsx
@@ -20,6 +20,8 @@ const UploadAntiques = () => {
           const image = form.image.value;
 
           const description = form.description.value;
+         
+          const materials = form.materials.value
 
           const selectedCategories = Array.from(form.categories.options)
                .filter((option) => option.selected)
@@ -170,17 +172,28 @@ const UploadAntiques = () => {
                     <div className="flex gap-8">
                          {/*material  */}
                          <div className="lg:w-1/2">
-                              <div className="form-group ">
+                              <div className="form-group relative ">
                                    {materials.map((material, index) => (
+                                       <>
                                         <input
                                              key={index}
                                              type="text"
+                                             id="materials"
+                                             name="materials"
                                              value={material}
                                              onChange={(event) => handleMaterialChange(index, event)}
                                              required
                                              placeholder="Materials"
-                                             className=" rounded-md w-full"
+                                             className=" block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                         />
+                                       <label
+                                        htmlFor="materials"
+                                        value="materials"
+                                        className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                                   >
+                                       materials
+                                   </label>
+                                       </>
                                    ))}
                               </div>
                          </div>
--- Frontend/src/shop/Shop.jsx
+++ Frontend/src/shop/Shop.jsx
@@ -13,8 +13,9 @@ const Shop = () => {
                <h2 className="text-5xl font-bold text-center">All Items</h2>
                <div className="grid gap-5 my-8 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1">
                     {arts.map((item, index) => {
+                         
                          return (
-                              <Link to="/admin/dashboard">
+                              <Link to="/admin/dashboard" key={index}>
                                    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700  text-blue-950">
                                         <a href="#">
                                              <div className="min-w-[250px] md:w-full flex-wrap">
@@ -77,12 +78,12 @@ const Shop = () => {
                                                        >
                                                             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                                                        </svg>
-                                                       <p>
-                                                            {" "}
+                                                       {/* <p>
+                                                          
                                                             <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
                                                                  {item.seller.rating}
                                                             </span>
-                                                       </p>
+                                                       </p> */}
                                                   </div>
                                                   {/* <div className="flex items-center justify-between"> */}
                                                   <p>
